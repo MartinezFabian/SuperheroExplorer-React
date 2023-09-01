c1c8f9d448e231880c37172eb3a2c5cba0598110
MartinezFabian <FabianMartinezDev@gmail.com>
2023-09-01 20:39:35 -0300

    Feat: Add HeroesRouter.jsx component

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,4 +1,4 @@
-export const DcPages = () => {
+export const DcPage = () => {
   return (
     <>
       <h1>DC</h1>
---
+++ src/heroes/routes/HeroesRouter.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar } from '../../ui/components/Navbar';
+import { MarvelPage } from '../pages/MarvelPage';
+import { DcPage } from '../pages/DcPage';
+import { SearchPage } from '../pages/SearchPage';
+import { NotFoundPage } from '../pages/NotFoundPage';
+
+export const HeroesRouter = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path="/marvel" element={<MarvelPage></MarvelPage>} />
+        <Route path="/dc" element={<DcPage></DcPage>} />
+        <Route path="/search" element={<SearchPage></SearchPage>} />
+        <Route path="/404" element={<NotFoundPage></NotFoundPage>} />
+        <Route path="/" element={<Navigate to="/marvel" />} /> {/* Redirige desde la ruta raíz */}
+        <Route path="/*" element={<Navigate to="/404" />} />
+      </Routes>
+    </>
+  );
+};
