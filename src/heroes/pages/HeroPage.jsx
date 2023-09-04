4e5867383d4e67b214fe02f0191896cbcfafb4f7
MartinezFabian <FabianMartinezDev@gmail.com>
2023-09-03 21:08:27 -0300

    Feat: Add HeroPage.module.css for component styles

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,8 @@
 import { Navigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers/getHeroById';
 
+import styles from './HeroPage.module.css';
+
 export const HeroPage = () => {
   const { id } = useParams();
 
@@ -15,32 +17,32 @@ export const HeroPage = () => {
   const heroImg = `/assets/img/${id}.jpg`;
 
   return (
-    <main>
-      <section>
-        <img src={heroImg} alt={superhero} />
+    <main className={styles.main}>
+      <section className={styles.main__section}>
+        <img src={heroImg} alt={superhero} className={styles.main__img} />
       </section>
 
-      <section>
-        <h2>{superhero}</h2>
+      <section className={styles.information}>
+        <h2 className={styles.information__heading}>{superhero}</h2>
 
         <div>
-          <p>
-            Alter Ego: <span>{alter_ego}</span>
+          <p className={styles.information__text}>
+            Alter Ego: <span className={styles.information__span}>{alter_ego}</span>
           </p>
-          <p>
-            Publisher: <span>{publisher}</span>
+          <p className={styles.information__text}>
+            Publisher: <span className={styles.information__span}>{publisher}</span>
           </p>
-          <p>
-            First Appearance: <span>{first_appearance}</span>
+          <p className={styles.information__text}>
+            First Appearance: <span className={styles.information__span}>{first_appearance}</span>
           </p>
         </div>
 
         <div>
-          <h3>Characters</h3>
+          <h3 className={styles.information__heading}>Characters</h3>
           <p>{characters}</p>
         </div>
 
-        <button>Back</button>
+        <button className={styles.information__btn}>Back</button>
       </section>
     </main>
   );
